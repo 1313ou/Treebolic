81515e156af6b466b1007f1e357375ae3b5c621f
bbou <bbou@ac-toulouse.fr>
2019-09-05 11:50:47 +0200

    donate, rate, others artwork toolchain

--- treebolic/artwork/make-artwork.sh
+++ treebolic/artwork/make-artwork.sh
@@ -3,17 +3,17 @@
 source "./lib-artwork.sh"
 
 launch="ic_launcher.svg ic_launcher_round.svg"
-web="ic_launcher.svg"
+app="ic_launcher.svg"
+logo="logo_app.svg"
 
 icon="ic_treebolic.svg"
 status="ic_status_*.svg"
 provider="ic_xml.svg ic_textindent.svg ic_textindenttre.svg ic_textpair.svg ic_refresh.svg"
 splash="ic_splash.svg"
 splash_text="ic_splash_text.svg"
-logo="logo_app.svg"
 
 make_mipmap "${launch}" 48
-make_app "${web}" 512
+make_app "${app}" 512
 make_res "${logo}" 48
 
 make_res "${icon}" 48
@@ -22,4 +22,4 @@ make_res "${provider}" 48
 make_res "${splash}" 144
 make_res "${splash_text}" 12
 
-check
+check_dir "${dirres}"
