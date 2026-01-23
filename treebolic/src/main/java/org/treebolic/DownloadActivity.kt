51ae357bfbbee4335e1d19b31b97f32d460cb061
1313ou <1313ou@gmail.com>
2026-01-23 17:31:20 +0100

    android.nonTransitiveRClass

--- treebolic/src/main/java/org/treebolic/DownloadActivity.kt
+++ treebolic/src/main/java/org/treebolic/DownloadActivity.kt
@@ -14,6 +14,7 @@ import org.treebolic.storage.Storage.getTreebolicStorage
 import java.io.File
 import java.io.IOException
 import java.io.InputStream
+import org.treebolic.download.R as DownloadR
 
 /**
  * Download activity
@@ -32,7 +33,7 @@ class DownloadActivity : org.treebolic.download.DownloadActivity() {
             downloadUrl = "$base/$file"
         }
         if (downloadUrl == null || downloadUrl!!.isEmpty()) {
-            Toast.makeText(this, R.string.error_null_download_url, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, DownloadR.string.error_null_download_url, Toast.LENGTH_SHORT).show()
             finish()
         }
     }
